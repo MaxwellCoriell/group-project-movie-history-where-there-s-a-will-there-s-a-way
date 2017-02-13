c7a8dc441049707204d890e650eb44a552ce7c7c
William A Sims III <willsims14@gmail.com>
2017-02-13 15:07:11 -0600

    Got getMovies() function working (without flitering by user)

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
 let $ = require('jquery');
+
+// Required modules
+let templates = require("../templates/movie-grid.hbs");
+let db = require("./db-interactions.js");
+let user = require("./user.js");
+
+
+
+
+function populateDOM(){
+	let currentUser = user.getUser();
+	db.getMovies(currentUser)
+	.then( function(movieData){
+		console.log("Movie Data: ", movieData);
+		// var idArray = Object.keys(movieData);
+		// idArray.forEach(function(key){
+		// 	movieData[key].id = key;
+		// });
+		// console.log("With Keys: ", movieData);
+		// templates.makeSongList(movieData);
+	});
+}
+
+
+
+
+// Login button onClick:
+$("#nav-login-link").click(function(){
+	user.logInGoogle()
+	.then( function(result){
+		user.setUser(result.user.uid);
+		populateDOM();
+	});
+});
