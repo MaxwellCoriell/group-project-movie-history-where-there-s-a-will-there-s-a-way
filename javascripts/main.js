7ef9203e0169c1ae9994ca482789ccd7421095bc
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-02-14 11:20:00 -0600

    added movie cards for suggested movie div

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,14 +13,21 @@ let user = require("./user.js");
 function populateDOM(){
 	let currentUser = user.getUser();
 	db.getMovies(currentUser)
-	.then( function(movieData){
-		console.log("Movie Data: ", movieData);
-		// var idArray = Object.keys(movieData);
-		// idArray.forEach(function(key){
-		// 	movieData[key].id = key;
-		// });
-		// console.log("With Keys: ", movieData);
-		// templates.makeSongList(movieData);
+	.then(function(movieData) {
+		// let movieActors = "";
+		for (var i = 0; i < movieData.length; i++) {
+			$("#suggested-movies").append(
+											`<section id="card-${movieData[i]}-${movieData[i].keys}" class="card-wrapper col-xs-4" >
+												<div class="innerCard" style="border: 2px solid black">
+												    <h3 class="movie-header"><strong>${movieData[i].title}</strong></h3>
+												    <h4 class="movie-year"><strong>${movieData[i].year}</strong></h4>
+												    <img src="${movieData[i].posterURL}" height="200" >
+												    <h4><strong>Leading Actors:</strong>${movieData[i].actors}</h4>
+												    <button type="button" value="add-to-my-movies">I want to see this movie</button>
+		    										<button type="button" value="add-to-my-watched-movies">I seen this movie</button>
+		    									</div>
+											</section>`);
+		}
 	});
 }
 
