aa1b22a2a6edb0a48a3e468b447f2a4ede59a931
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-02-15 09:08:13 -0600

    clicking add button adds correct object to firebase db. However, this breaks the loop that populates the page.

--- dist/app.js
+++ dist/app.js
@@ -5,73 +5,69 @@ let $ = require('jquery');
 let db = require("./db-interactions.js");
 let user = require("./user.js");
 
+// function that adds searched movie cards
 function addSuggested(movieData) {
+    console.log('addSuggested initiated');
+    console.log('movieData.length = ', movieData.length);
+    var currentUser = user.getUser();
     for (var i = 0; i < movieData.length; i++) {
+        console.log('movieData[i] = ', movieData[i]);
+        if (movieData[i].userID === currentUser) {
+            $("#my-movies").append(`<p>movieData[i]</p>`);
+        } else {
         $("#suggested-movies").append(
                                         `<section id="card-${movieData[i]}" class="card-wrapper col-xs-4" >
                                             <div class="innerCard" style="border: 2px solid black">
-                                                <h3 class="movie-header"><strong>${movieData[i].title}</strong></h3>
-                                                <h4 class="movie-year"><strong>${movieData[i].year}</strong></h4>
+                                                <h3 class="movie-header">${movieData[i].title}</h3>
+                                                <h4 class="movie-year">${movieData[i].year}</h4>
                                                 <img src="${movieData[i].posterURL}" height="200" >
-                                                <h4><strong>Leading Actors:</strong>${movieData[i].actors}</h4>
+                                                <h5>${movieData[i].actors}</h5>
                                                 <button type="button" class="add-to-my-movies" value="${movieData[i].title}">I want to see this movie</button>
                                                 <button type="button" class="add-to-my-watched-movies" value="add-to-my-watched-movies">I seen this movie</button>
                                             </div>
                                         </section>`);
+        }
     }
-    $(".add-to-my-watched-movies").click(addToWatched);
+    // $(".add-to-my-watched-movies").click(addToWatched);
     $(".add-to-my-movies").click(addToMyMovies);
 }
 
-
+// function that adds movie to the database
 function addToMyMovies() {
     console.log('you clicked I want to see this movie');
-    console.log('closest title:', $(event.currentTarget).closest("h3"));
+    var currentCard = $(event.currentTarget);
+    console.log('url:', currentCard.siblings("img").attr("src"));
     var currentUser = user.getUser();
-    var testMovie = {
-        "title":"Movie",
-        "year":"1999",
-        "actors":["list", "of", "actors"],
+    var myMovie = {
+        "title": currentCard.siblings("h3").html(),
+        "year": currentCard.siblings("h4").html(),
+        "actors": currentCard.siblings("h5").html(),
         "userID": currentUser,
         "rating": "",
-        "plot":"awesome",
-        "posterURL": "https://images-na.ssl-images-amazon.com/images/M/MV5BMWEzNmUxZTMtZjY0My00OGNmLWIyNDctODM2YzZjM2YwZWEwXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+        "posterURL": currentCard.siblings("img").attr("src")
     };
-    // return new Promise (function(resolve, reject) {
-    //     $.ajax({
-    //         url: "https://movie-history-6e707.firebaseio.com/movies.json",
-    //         type: "POST",
-    //         data: JSON.stringify(testMovie),
-    //         dataType: "json"
-    //     }).done(function(movie) {
-    //         resolve(movie);
-    //     });
-    // });
-}
-
-function addToWatched() {
-    addToMyMovies()
-    .then(function(){
-
+    return new Promise (function(resolve, reject) {
+        $.ajax({
+            url: "https://movie-history-6e707.firebaseio.com/movies.json",
+            type: "POST",
+            data: JSON.stringify(myMovie),
+            dataType: "json"
+        }).done(function(movie) {
+            resolve(movie);
+        });
     });
-    console.log('you clicked I seen this movie');
 }
+
+// function addToWatched() {
+//     addToMyMovies()
+//     .then(function(){
+
+//     });
+//     console.log('you clicked I seen this movie');
+// }
 
 module.exports = {addSuggested};
 
-// function addSong(songFormObj) {
-//     console.log('addSong =', songFormObj);
-//     return new Promise (function(resolve, reject) {
-//         $.ajax({
-//             url: "https://movie-history-6e707.firebaseio.com/movies.json",
-//             type: "POST",
-//             data: JSON.stringify(songFormObj),
-//             dataType: "json"
-//         }).done(function(songId) {
-//             resolve(songId);
-//         });
-//     });
-// }
 },{"./db-interactions.js":2,"./user.js":6,"jquery":30}],2:[function(require,module,exports){
 'use strict';
 
